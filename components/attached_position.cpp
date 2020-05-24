825b5d9acec3edb7a857246d546338a2926a8d12
ZamfirYonchev <zamfir.yonchev@gmail.com>
2020-05-24 19:24:54 +0300

    added absolute_or_scaled handling of offsets

--- components/attached_position.cpp
+++ components/attached_position.cpp
@@ -7,12 +7,14 @@
 
 #include "attached_position.h"
 #include "../globals.h"
+#include "../utilities.h"
 
 double AttachedPosition::x() const
 {
 	if(entity_system().entity(m_attached_id))
 	{
-	    return entity_system().entity(m_attached_id)->position()->x() + m_offset_x;
+	    return entity_system().entity(m_attached_id)->position()->x()
+	    	 + absolute_or_scaled(m_offset_x, entity_system().entity(m_attached_id)->position()->w());
 	}
 	else
 	{
@@ -25,7 +27,8 @@ double AttachedPosition::y() const
 {
 	if(entity_system().entity(m_attached_id))
 	{
-		return entity_system().entity(m_attached_id)->position()->y() + m_offset_y;
+		return entity_system().entity(m_attached_id)->position()->y()
+		+ absolute_or_scaled(m_offset_y, entity_system().entity(m_attached_id)->position()->h());
 	}
 	else
 	{
@@ -38,7 +41,8 @@ double AttachedPosition::w() const
 {
 	if(entity_system().entity(m_attached_id))
 	{
-		return entity_system().entity(m_attached_id)->position()->w() + m_offset_w;
+		return entity_system().entity(m_attached_id)->position()->w()
+			 + absolute_or_scaled(m_offset_w, entity_system().entity(m_attached_id)->position()->w());
 	}
 	else
 	{
@@ -51,7 +55,8 @@ double AttachedPosition::h() const
 {
 	if(entity_system().entity(m_attached_id))
 	{
-		return entity_system().entity(m_attached_id)->position()->h() + m_offset_h;
+		return entity_system().entity(m_attached_id)->position()->h()
+			 + absolute_or_scaled(m_offset_h, entity_system().entity(m_attached_id)->position()->h());
 	}
 	else
 	{
