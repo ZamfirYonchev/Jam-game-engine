d40de308aeb971f30269d275875b583121bd599d
ZamfirYonchev <zamfir.yonchev@gmail.com>
2020-05-31 12:32:07 +0300

    added constexpr to absolute_or_scaled, added inline function resolved_entity for negative entity ids

--- utilities.h
+++ utilities.h
@@ -11,9 +11,14 @@
 #include "globals.h"
 #include "math_ext.h"
 
-inline double absolute_or_scaled(double val, double scale)
+constexpr double absolute_or_scaled(double val, double scale)
 {
 	return val*(is_normalized(val)*(scale-1)+1);
+}
+
+inline EntityID resolved_entity(EntityID in_entity_id)
+{
+	return (in_entity_id >= 0)*in_entity_id + (in_entity_id < 0)*entity_system().previous_entity(1-in_entity_id);
 }
 
 #endif /* UTILITIES_H_ */
