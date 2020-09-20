362dc398b2be542fc5a1efd9e4a56df9b286afaf
ZamfirYonchev <zamfir.yonchev@gmail.com>
2020-09-20 20:41:24 +0300

    removed resolved_entity function

--- utilities.h
+++ utilities.h
@@ -16,11 +16,6 @@ constexpr double absolute_or_scaled(double val, double scale)
 	return val*(is_normalized(val)*(scale-1)+1);
 }
 
-inline EntityID resolved_entity(EntityID in_entity_id)
-{
-	return EntityID{(in_entity_id >= 0)*in_entity_id + (in_entity_id < 0)*entity_system().previous_entity_id(EntityID{1-in_entity_id})};
-}
-
 constexpr unsigned long hash(const char* str)
 {
     unsigned long hash = 5381;
