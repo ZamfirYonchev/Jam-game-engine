473a3972af17e7189e60e355dc28817d7f238ef6
ZamfirYonchev <zamfir.yonchev@gmail.com>
2020-10-11 10:57:25 +0300

    added discard_if algorithm for sets

--- utilities.h
+++ utilities.h
@@ -10,6 +10,7 @@
 
 #include "globals.h"
 #include "math_ext.h"
+#include <set>
 
 constexpr double absolute_or_scaled(double val, double scale)
 {
@@ -25,6 +26,18 @@ constexpr unsigned long hash(const char* str)
     }
 
     return hash;
+}
+
+template <class T, class Comp, class Alloc, class Predicate>
+void discard_if(std::set<T, Comp, Alloc>& c, Predicate pred) {
+    for (auto it{std::begin(c)}, end{std::end(c)}; it != end; ) {
+        if (pred(*it)) {
+            it = c.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
 }
 
 #endif /* UTILITIES_H_ */
