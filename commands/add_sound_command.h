9034407dd4f53e14533d6b9e29c1106de250840f
ZamfirYonchev <zamfir.yonchev@gmail.com>
2020-12-27 16:34:20 +0200

    add and play sound commands take number of loops as a parameter

--- commands/add_sound_command.h
+++ commands/add_sound_command.h
@@ -19,18 +19,20 @@ struct Globals;
 class AddSoundCommand
 {
 public:
-	AddSoundCommand(const std::string& file)
+	AddSoundCommand(const std::string& file, const int repeat)
 	: m_file(file)
+	, m_repeat(repeat)
 	{}
 
     template<typename EntitySystemT, typename CommandSystemT, typename AllSystemsT>
     void operator()(EntitySystemT& entity_system, ResourceSystem& resource_system, InputSystem& input_system, CommandSystemT& command_system, RenderingSystem& rendering_system, AllSystemsT& all_systems, Globals& globals) const
     {
-    	resource_system.addNewSound(m_file);
+    	resource_system.addNewSound(m_file, m_repeat);
     }
 
 private:
     std::string m_file;
+    int m_repeat;
 };
 
 
--- commands/play_sound_command.h
+++ commands/play_sound_command.h
@@ -20,14 +20,12 @@ struct Globals;
 class PlaySoundCommand
 {
 public:
-	PlaySoundCommand(const SoundID sound_id, const int loops, const int channel)
+	PlaySoundCommand(const SoundID sound_id, const int channel)
 	: m_sound_id(sound_id)
-	, m_loops(loops)
 	, m_channel(channel)
 	{}
 
-	PlaySoundCommand(const SoundID sound_id, const int loops) : PlaySoundCommand(sound_id, loops, -1) {}
-	PlaySoundCommand(const SoundID sound_id) : PlaySoundCommand(sound_id, -1, -1) {}
+	PlaySoundCommand(const SoundID sound_id) : PlaySoundCommand(sound_id, -1) {}
 
     template<typename EntitySystemT, typename CommandSystemT, typename AllSystemsT>
     void operator()(EntitySystemT& entity_system, ResourceSystem& resource_system, InputSystem& input_system, CommandSystemT& command_system, RenderingSystem& rendering_system, AllSystemsT& all_systems, Globals& globals) const
@@ -35,7 +33,7 @@ public:
     	const auto sound_optional = resource_system.sound(m_sound_id);
     	if(sound_optional)
     	{
-    		if(Mix_PlayChannel(m_channel, sound_optional->sound(), m_loops) < 0)
+    		if(Mix_PlayChannel(m_channel, sound_optional->sound(), sound_optional->repeat()) < 0)
     		{
     			std::cerr << "Cannot play sound " << m_sound_id << ": " << Mix_GetError() << '\n';
     		}
@@ -48,7 +46,7 @@ public:
 
 private:
     SoundID m_sound_id;
-    int m_loops, m_channel;
+    int m_channel;
 };
 
 #endif /* COMMANDS_PLAY_SOUND_COMMAND_H_ */
