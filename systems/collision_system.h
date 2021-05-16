b87e6b2ca4a8fb97387f71a57d05e0ef61a10eaf
ZamfirYonchev <zamfir.yonchev@gmail.com>
2021-05-17 00:26:59 +0300

    fixed collision correction by dispositioning based on weight, assumes immovable objects have infinite weight

--- systems/collision_system.h
+++ systems/collision_system.h
@@ -24,6 +24,7 @@ template<typename EntitySystemT>
 class CollisionSystem : public SystemBase
 {
 public:
+    constexpr static double GRAVITY_ACCEL = -0.00980665; //always (IRL, 9.80665m/s^2 = 0.00980665mm/ms^2)
 
 	CollisionSystem(EntitySystemT& entity_system) : m_entity_system(entity_system) {}
 
@@ -33,14 +34,14 @@ public:
 
 		for(const auto entity_id : entities)
 		{
-			Interaction& interaction = m_entity_system.entity_component(entity_id, Interaction::null);
+			Interaction& interaction = m_entity_system.template entity_component<Interaction>(entity_id);
 			interaction.update_last_triggered_groups();
 		}
 
 		//register+unregister entities from regions
 		for(const auto entity_id : entities)
 		{
-			const Position& position = m_entity_system.entity_component(entity_id, Position::null);
+			const Position& position = m_entity_system.template entity_component<Position>(entity_id);
 			if(position)
 			{
 				const Collision::RegionLocation& old_location = regions_per_entity[entity_id];
@@ -75,20 +76,21 @@ public:
 
 		for(const auto entity_id : entities)
 		{
-			const auto& collision = m_entity_system.entity_component(entity_id, Collision::null);
-			if(collision.state() == Collision::CollisionState::MOVEABLE)
+			//const auto& collision = m_entity_system.template entity_component<Collision>(entity_id);
+			//TODO if(collision.state() == Collision::CollisionState::MOVEABLE)
 				collision_correction.insert(std::make_pair(entity_id, CorrectionValues{}));
 		}
 
 		for(const auto id0 : entities)
 		{
-			auto& collision0 = m_entity_system.entity_component(id0, Collision::null);
+			auto& collision0 = m_entity_system.template entity_component<Collision>(id0);
 			if(collision0)
 			{
-				auto& interaction0 = m_entity_system.entity_component(id0, Interaction::null);
-				const auto& position0 = m_entity_system.entity_component(id0, Position::null);
+				auto& interaction0 = m_entity_system.template entity_component<Interaction>(id0);
+				const auto& position0 = m_entity_system.template entity_component<Position>(id0);
 
 				collision0.set_standing_on(Collision::SurfaceType::AIR);
+
 				const Collision::RegionLocation& location0 = regions_per_entity[id0];
 
 				std::unordered_set<EntityID> near_entities;
@@ -100,15 +102,15 @@ public:
 
 				near_entities.erase(id0); //remove self from the set
 
-				const double x1 = position0.x() + position0.w();
-				const double y1 = position0.y() + position0.h();
+				const auto& movement0 = m_entity_system.template entity_component<Movement>(id0);
+				CorrectionValues& correction_values = collision_correction[id0];
 
 				for(const auto id1 : near_entities)
 				{
-					const auto& collision1 = m_entity_system.entity_component(id1, Collision::null);
+					const auto& collision1 = m_entity_system.template entity_component<Collision>(id1);
 					if(collision1 && id1 != id0)
 					{
-						const auto& position1 = m_entity_system.entity_component(id1, Position::null);
+						const auto& position1 = m_entity_system.template entity_component<Position>(id1);
 
 						if(objects_collide(position0.x(), position0.y(), position0.w(), position0.h()
 										 , position1.x(), position1.y(), position1.w(), position1.h()
@@ -116,7 +118,7 @@ public:
 						  )
 						{
 							//entities collide
-							const auto& interaction1 = m_entity_system.entity_component(id1, Interaction::null);
+							const auto& interaction1 = m_entity_system.template entity_component<Interaction>(id1);
 
 							interaction0.set_triggered_groups(interaction1.group_vector());
 
@@ -132,57 +134,92 @@ public:
 									procedure_calls.emplace_back(id0, interaction1.proc_id_other());
 							}
 
-							m_entity_system.entity_component(id0, Health::null).mod_hp_change(-collision1.on_collision_damage()*time_diff);
+							m_entity_system.template entity_component<Health>(id0).mod_hp_change(-collision1.on_collision_damage()*time_diff);
 
 							const bool entity0_correctable = (collision0.state() == Collision::CollisionState::MOVEABLE)
 														  && (collision1.state() >= Collision::CollisionState::MOVEABLE);
 
 							if(entity0_correctable)
 							{
-								const auto& movement0 = m_entity_system.entity_component(id0, Movement::null);
-								const auto& movement1 = m_entity_system.entity_component(id1, Movement::null);
+								const auto& movement1 = m_entity_system.template entity_component<Movement>(id1);
+								const double weight_coeff = movement1.mass()/(movement0.mass()+movement1.mass());
+
 								const double dx = movement1.dx() - movement0.dx();
 								const double dy = movement1.dy() - movement0.dy();
 
-								const double x2 = position1.x();
-								const double y2 = position1.y();
+								/*          __________________sw,sh
+								 *         |                  |
+								 *         |                  |
+								 *      h0 |                  |
+								 *         |_________         |
+								 *         |         |        |
+								 *     ____|___pxpy  |        |
+								 *    | h1 |   |     |        |
+								 *    |    |___|_____|________|
+								 * h0 |  0,0   |w1       w0
+								 *    |________|
+								 * x0y0   w0
+								 */
+
 								const double sw = position1.w() + position0.w();
 								const double sh = position1.h() + position0.h();
+								const double px = position0.x() + position0.w() - position1.x();
+								const double py = position0.y() + position0.h() - position1.y();
 
-								const double dvx = 2*(movement1.vx()-movement0.vx())*((collision1.state()==Collision::CollisionState::SOLID) ? 1 : movement1.mass()/(movement0.mass()+movement1.mass()))*collision0.elasticity()*collision1.elasticity();
-								const double dvy = 2*(movement1.vy()-movement0.vy())*((collision1.state()==Collision::CollisionState::SOLID) ? 1 : movement1.mass()/(movement0.mass()+movement1.mass()))*collision0.elasticity()*collision1.elasticity();
+								const double overlap_corr = min(min(px, sw-px), min(py, sh-py));
 
-								collision_correction[id0].vy += dvy;
-								collision_correction[id0].vx += dvx;
+								const double overlap_corr_x = (overlap_corr == px) ? -px :
+															  (overlap_corr == sw-px) ? sw-px : 0;
+
+								const double overlap_corr_y = (overlap_corr == py) ? -py :
+															  (overlap_corr == sh-py) ? sh-py : 0;
+
+								const double dvx_half = (movement1.vx()-movement0.vx())*weight_coeff;
+								const double dvy_half = (movement1.vy()-movement0.vy())*weight_coeff;
+
+								correction_values.x += overlap_corr_x*weight_coeff;
+								correction_values.y += overlap_corr_y*weight_coeff;
 
 								if(dy > 0)
 								{
-									const double t = clip(lines_cross(x1, y1, dx, dy, x2, y2+sh, sw, 0), -1.0, 1.0);
+									const double t = lines_cross(px, py, dx, dy, 0, sh, sw, 0);
+
 									if(t >= 0.0)
 									{
-										collision_correction[id0].y = t*((collision1.state()==Collision::CollisionState::SOLID)*movement1.dy() - movement0.dy());
+										correction_values.vy += dvy_half*(1+collision0.elasticity()*collision1.elasticity());
+										correction_values.vx += dvx_half*2*movement0.friction_x()*movement1.friction_x();
+
 										collision0.set_standing_on(Collision::SurfaceType::GROUND);
 									}
 								}
 								else if(dy < 0)
 								{
-									const double t = lines_cross(x1, y1, dx, dy, x2, y2, sw, 0);
+									const double t = lines_cross(px, py, dx, dy, 0, 0, sw, 0);
 									if(t >= 0.0)
-										collision_correction[id0].y = t*((collision1.state()==Collision::CollisionState::SOLID)*movement1.dy() - movement0.dy());
+									{
+										correction_values.vy += dvy_half*(1+collision0.elasticity()*collision1.elasticity());
+										correction_values.vx += dvx_half*2*movement0.friction_x()*movement1.friction_x();
+									}
 								}
 
 
 								if(dx < 0)
 								{
-									const double t = clip(lines_cross(x1, y1, dx, dy, x2, y2, 0, sh), -1.0, 1.0);
+									const double t = lines_cross(px, py, dx, dy, 0, 0, 0, sh);
 									if(t >= 0.0)
-										collision_correction[id0].x = t*((collision1.state()==Collision::CollisionState::SOLID)*movement1.dx()- movement0.dx());
+									{
+										correction_values.vx += dvx_half*(1+collision0.elasticity()*collision1.elasticity());
+										correction_values.vy += dvy_half*2*movement0.friction_y()*movement1.friction_y();
+									}
 								}
 								else if(dx > 0)
 								{
-									const double t = clip(lines_cross(x1, y1, dx, dy, x2+sw, y2, 0, sh), -1.0, 1.0);
+									const double t = lines_cross(px, py, dx, dy, sw, 0, 0, sh);
 									if(t >= 0.0)
-										collision_correction[id0].x = t*((collision1.state()==Collision::CollisionState::SOLID)*movement1.dx() - movement0.dx());
+									{
+										correction_values.vx += dvx_half*(1+collision0.elasticity()*collision1.elasticity());
+										correction_values.vy += dvy_half*2*movement0.friction_y()*movement1.friction_y();
+									}
 								}
 							}
 						}
@@ -198,8 +235,8 @@ public:
 
 		for(const auto& entity_pair : collision_correction)
 		{
-			auto& position = m_entity_system.entity_component(entity_pair.first, Position::null);
-			auto& movement = m_entity_system.entity_component(entity_pair.first, Movement::null);
+			auto& movement = m_entity_system.template entity_component<Movement>(entity_pair.first);
+			auto& position = m_entity_system.template entity_component<Position>(entity_pair.first);
 			position.mod_x(entity_pair.second.x);
 			position.mod_y(entity_pair.second.y);
 			movement.mod_dx(entity_pair.second.x);
@@ -210,7 +247,7 @@ public:
 
 		for(const auto id : entities)
 		{
-			const Interaction& interaction = m_entity_system.entity_component(id, Interaction::null);
+			const Interaction& interaction = m_entity_system.template entity_component<Interaction>(id);
 			if(interaction)
 			{
 				const bool last_triggered = (interaction.last_triggered_groups() >> interaction.trigger_group())%2;
@@ -273,8 +310,10 @@ private:
     struct CorrectionValues
     {
     	CorrectionValues() : x{0}, y{0}, vx{0}, vy{0} {}
+
     	double x, y, vx, vy;
     };
+
 
     EntitySystemT& m_entity_system;
     std::unordered_map<RegionPosition, std::unordered_set<EntityID>, RegionPositionHashFn> entities_per_region;
