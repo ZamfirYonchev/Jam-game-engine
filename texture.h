7d91b99394f30ae073ef9524d1eb61463adf9ca6
ZamfirYonchev <zamfir.yonchev@gmail.com>
2022-01-15 11:36:11 +0200

    removed load_from_string and load_from_file builder methods and started using constructors instead

--- texture.h
+++ texture.h
@@ -17,9 +17,43 @@
 class Texture
 {
 public:
-	Texture(std::string_view filename, SDL_Renderer* renderer) : m_texture(nullptr)
+	Texture(std::string_view filename, SDL_Renderer& renderer) : m_texture{nullptr}
 	{
-		load_from_file(filename, renderer);
+        SDL_Surface* surface = IMG_Load(filename.data());
+
+        if(surface == nullptr)
+        {
+            std::cerr << "Cannot load image " << filename << "! Error: " << IMG_GetError() << std::endl;
+        }
+        else
+        {
+            SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+            m_texture = SDL_CreateTextureFromSurface(&renderer, surface);
+            if(m_texture == nullptr)
+            {
+                std::cerr << "Cannot create texture from " << filename << "! Error: " << SDL_GetError() << std::endl;
+            }
+            SDL_FreeSurface(surface);
+        }
+	}
+
+	Texture(std::string_view text, Font& font, uint8_t r, uint8_t g, uint8_t b, SDL_Renderer& renderer) : m_texture{nullptr}
+	{
+        SDL_Surface* surface = TTF_RenderText_Solid(font.font(), text.data(), SDL_Color{r, g, b, 0});
+
+        if(surface == nullptr)
+        {
+            std::cerr << "Cannot render string '" << text << "'! Error: " << TTF_GetError() << std::endl;
+        }
+        else
+        {
+            m_texture = SDL_CreateTextureFromSurface(&renderer, surface);
+            if(m_texture == nullptr)
+            {
+                std::cerr << "Cannot create texture for string '" << text << "'! Error: " << SDL_GetError() << std::endl;
+            }
+            SDL_FreeSurface(surface);
+        }
 	}
 
 	Texture() : m_texture(nullptr) {}
@@ -43,49 +77,6 @@ public:
     	rhs.m_texture = nullptr;
 
     	return *this;
-    }
-
-    Texture& load_from_file(std::string_view file, SDL_Renderer* renderer)
-    {
-        unload();
-        SDL_Surface* surface = IMG_Load(file.data());
-
-        if(surface == nullptr)
-        {
-            std::cerr << "Cannot load image " << file << "! Error: " << IMG_GetError() << std::endl;
-        }
-        else
-        {
-            SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-            m_texture = SDL_CreateTextureFromSurface(renderer, surface);
-            if(m_texture == nullptr)
-            {
-                std::cerr << "Cannot create texture from " << file << "! Error: " << SDL_GetError() << std::endl;
-            }
-            SDL_FreeSurface(surface);
-        }
-        return *this;
-    }
-
-    Texture& load_from_string(std::string_view text, Font& font, uint8_t r, uint8_t g, uint8_t b, SDL_Renderer* renderer)
-    {
-        unload();
-        SDL_Surface* surface = TTF_RenderText_Solid(font.font(), text.data(), SDL_Color{r, g, b, 0});
-
-        if(surface == nullptr)
-        {
-            std::cerr << "Cannot render string '" << text << "'! Error: " << TTF_GetError() << std::endl;
-        }
-        else
-        {
-            m_texture = SDL_CreateTextureFromSurface(renderer, surface);
-            if(m_texture == nullptr)
-            {
-                std::cerr << "Cannot create texture for string '" << text << "'! Error: " << SDL_GetError() << std::endl;
-            }
-            SDL_FreeSurface(surface);
-        }
-        return *this;
     }
 
     void unload() noexcept
