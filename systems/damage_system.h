74ccf7367d2f8dc91f561a02c80317b997bee3a9
ZamfirYonchev <zamfir.yonchev@gmail.com>
2022-01-30 21:35:58 +0200

    added an unordered_set of alive entities to better track lifetime

--- systems/damage_system.h
+++ systems/damage_system.h
@@ -33,11 +33,14 @@ public:
 			auto& health = m_entity_system.template entity_component<HealthT>(id);
 	    	if(health)
 	    	{
-				const bool was_alive = health.alive();
+				const bool was_alive = alive_entities.find(id) != std::cend(alive_entities);
 
 				health.update_health(time_diff);
 				if(was_alive && health.alive() == false && health.on_death_exec() > 0)
 					external_commands << "Select " << id << " Call " << health.on_death_exec() << '\n';
+
+				if(health.alive() && was_alive == false) alive_entities.insert(id);
+				else if(health.alive() == false && was_alive) alive_entities.erase(id);
 	    	}
 	    	else
 	    	{
@@ -59,6 +62,11 @@ public:
     			remove_id(id);
     		else if(change > 0)
     			add_id(id);
+
+    		if(m_entity_system.template entity_component<HealthT>(id).alive())
+				alive_entities.insert(id);
+    		else
+				alive_entities.erase(id);
 		}
     }
 
@@ -66,6 +74,7 @@ private:
     EntitySystemT& m_entity_system;
     Globals& globals;
     std::stringstream& external_commands;
+    std::unordered_set<EntityID> alive_entities;
 };
 
 #endif /* SYSTEMS_DAMAGE_SYSTEM_H_ */
